928cf4e7d1e40d70cf1be5fb36bd53ab4e453a03
AlesDod02 <alessandro.dod@gmail.com>
2023-11-15 14:22:37 +0100

    Bonus html

--- js/main.js
+++ js/main.js
@@ -1,19 +1,30 @@
+let lista = document.getElementById("listanumeri");
+
 
 for (let i=1; i < 101; i++) {
+    let listItem = document.createElement("li")
     
     if (i%15==0){
+        listItem.textContent="fizzbuzz"
         //i=fizzbuzz
         console.log("fizzbuzz")
     }
     else if(i%5==0){
+        listItem.textContent="buzz"
         //i=buzz
         console.log("buzz");
     }
     else if(i%3==0){
+        listItem.textContent="fizz"
         //i=fizz
         console.log("fizz");
     }
     else {
-        console.log(i);
+        console.log(i)
+        listItem.textContent=i
     }
+    listanumeri.appendChild(listItem)
+   
 }
+
+
